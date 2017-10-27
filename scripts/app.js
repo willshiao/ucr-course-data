91c0835c22dacff0bfd34fad5bbb22ec0224df07
William Shiao <willshiao@gmail.com>
2017-10-26 18:29:35 -0700

    Insert catalog items into database

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,47 @@
 
 const mongoose = require('mongoose')
 const config = require('config')
-mongoose.Promise = require('bluebird')
+const _ = require('lodash')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+mongoose.Promise = Promise
 
+const getCatalog = require('./catalog.js')
 const logger = require('./lib/logger')
+const fileHelper = require('./lib/fileHelper')
+const Course = require('./models/Course')
+const Faculty = require('./models/Faculty')
 
-mongoose.createConnection(config.get('db.uri'), config.get('db.options'))
-  .then(() => {
-    logger.debug('Connected to database.')
+async function mockFetch () {
+  const filename = fileHelper.getCatalogPath()
+  const data = await fs.readFileAsync(filename)
+  return JSON.parse(data)
+}
+
+async function fetchData () {
+  const catalog = await getCatalog()
+  console.log(catalog)
+}
+
+(async function () {
+  await mongoose.connect(config.get('db.uri'), config.get('db.options'))
+  logger.debug('Connected to database.')
+  const catalog = await mockFetch()
+  logger.debug('Fetched data')
+
+  const chunks = _.chunk(catalog, 200)
+
+  console.time('insertion')
+  await Promise.each(chunks, async chunk => {
+    await Promise.map(chunk, async item => {
+      item.faculty = await Promise.map(item.faculty, async faculty => {
+        const doc = await Faculty.findOrCreate({ bannerId: faculty.bannerId }, _.omit(faculty, 'bannerId'), faculty)
+        return doc.doc._id || null
+      })
+      return item
+    })
+    await Course.create(chunk)
   })
+  console.timeEnd('insertion')
+  logger.debug('Inserted data')
+})()
--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,17 +1,16 @@
-'use strict';
+'use strict'
 
-const config = require('config');
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
-const assert = require('assert');
+const config = require('config')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+const assert = require('assert')
 
-const fileHelper = require('./lib/fileHelper');
-const authHandler = require('./lib/authHandler');
-const rp = require('./lib/request');
+const fileHelper = require('./lib/fileHelper')
+const authHandler = require('./lib/authHandler')
+const rp = require('./lib/request')
 
-
-async function getCatalog(j) {
-  const jar = j || await authHandler.getJar();
+async function getCatalog (j) {
+  const jar = j || await authHandler.getJar()
 
   const options = {
     uri: config.get('catalog.urls.search'),
@@ -19,58 +18,47 @@ async function getCatalog(j) {
       txt_term: config.get('catalog.term'),
       txt_subject: config.get('catalog.subjects'),
       pageOffset: 0,
-      pageMaxSize: 1,
+      pageMaxSize: 1
     },
     jar,
-    json: true,
-  };
+    json: true
+  }
 
   // First, get the total number of courses
-  let res = await rp(options);
-  const numCourses = res.totalCount;
-  console.log('Courses:', numCourses);
-  assert.notEqual(numCourses, 0);
+  let res = await rp(options)
+  const numCourses = res.totalCount
+  console.log('Courses:', numCourses)
+  assert.notEqual(numCourses, 0)
 
   // Next, fetch all courses
-  let fetchedCourses = [];
-  const requestQueue = [];
-  options.qs.pageMaxSize = 500;
+  let fetchedCourses = []
+  const requestQueue = []
+  options.qs.pageMaxSize = 500
 
-  for(let pageOffset = 0; pageOffset <= numCourses; pageOffset += 500) {
+  for (let pageOffset = 0; pageOffset <= numCourses; pageOffset += 500) {
     requestQueue.push((async () => {
-      const settings = options;
-      settings.qs.pageOffset = pageOffset;
-      res = await rp(settings);
-      console.log('Fetched', res.data.length, 'courses');
-      fetchedCourses = fetchedCourses.concat(res.data);
-    })());
+      const settings = options
+      settings.qs.pageOffset = pageOffset
+      res = await rp(settings)
+      console.log('Fetched', res.data.length, 'courses')
+      fetchedCourses = fetchedCourses.concat(res.data)
+    })())
   }
-  await Promise.all(requestQueue);
-  console.log('Fetched a total of', fetchedCourses.length, 'courses');
+  await Promise.all(requestQueue)
+  console.log('Fetched a total of', fetchedCourses.length, 'courses')
 
-  // Next, get one of each lecture
-  const courses = {};
-  fetchedCourses.forEach((course) => {
-    if(course.scheduleTypeDescription !== 'Lecture') return;
-    if(!(course.subjectCourse in courses)) {
-      courses[course.subjectCourse] = course;
-      delete courses[course.subjectCourse].meetingsFaculty; // Delete for privacy reasons
-    }
-  });
-
-  return courses;
+  return fetchedCourses
 }
 
-async function main() {
-  const filename = fileHelper.getCatalogPath();
-  const catalog = await getCatalog();
+async function main () {
+  const filename = fileHelper.getCatalogPath()
+  const catalog = await getCatalog()
 
-  await fs.writeFileAsync(filename, JSON.stringify(catalog, null, 2));
+  await fs.writeFileAsync(filename, JSON.stringify(catalog, null, 2))
 }
 
-
-if(require.main === module) { // Was run directly
-  main();
+if (require.main === module) { // Was run directly
+  main()
 } else { // Required as module
-  module.exports = getCatalog;
+  module.exports = getCatalog
 }
